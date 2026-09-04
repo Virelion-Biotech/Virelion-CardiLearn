022f63e611b3abcb736c264f8634fbc627af403f
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-04 11:26:52 +0300

    Expose Step 18 reproducibility API

--- cardilearn/__init__.py
+++ cardilearn/__init__.py
@@ -3,20 +3,42 @@
 __version__ = "0.3.0"
 
 from .benchmark_protocol import BenchmarkSpec, compare_seeded_scores, rank_models, summarize_repeated_scores
+from .config_loader import ConfigError, load_yaml_config, validate_reproducibility_config
 from .dataset_card import DatasetCard
 from .registry import ModelRegistry
+from .reproducibility import (
+    ReproducibilityManifest,
+    config_fingerprint,
+    dataframe_fingerprint,
+    fingerprint_ids,
+    fingerprint_mapping,
+    load_manifest,
+    make_manifest,
+    save_manifest,
+)
 from .schema import DatasetSpec, FeatureManifest
 from .validation import IntegrityReport, validate_dataset
 
 __all__ = [
     "BenchmarkSpec",
+    "ConfigError",
     "DatasetCard",
     "DatasetSpec",
     "FeatureManifest",
     "IntegrityReport",
     "ModelRegistry",
+    "ReproducibilityManifest",
     "compare_seeded_scores",
+    "config_fingerprint",
+    "dataframe_fingerprint",
+    "fingerprint_ids",
+    "fingerprint_mapping",
+    "load_manifest",
+    "load_yaml_config",
+    "make_manifest",
     "rank_models",
+    "save_manifest",
     "summarize_repeated_scores",
     "validate_dataset",
+    "validate_reproducibility_config",
 ]
