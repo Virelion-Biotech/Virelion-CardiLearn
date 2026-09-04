0c5000e5ca0c5e39f000bcedcd03c40fde826e36
Syed Umer Hannan <syedumerhannan@icloud.com>
2026-09-04 03:05:01 +0300

    Expose Step 15 benchmark protocol API

--- cardilearn/__init__.py
+++ cardilearn/__init__.py
@@ -2,9 +2,21 @@
 
 __version__ = "0.3.0"
 
+from .benchmark_protocol import BenchmarkSpec, compare_seeded_scores, rank_models, summarize_repeated_scores
 from .dataset_card import DatasetCard
 from .registry import ModelRegistry
 from .schema import DatasetSpec, FeatureManifest
 from .validation import IntegrityReport, validate_dataset
 
-__all__ = ["DatasetCard", "DatasetSpec", "FeatureManifest", "IntegrityReport", "ModelRegistry", "validate_dataset"]
+__all__ = [
+    "BenchmarkSpec",
+    "DatasetCard",
+    "DatasetSpec",
+    "FeatureManifest",
+    "IntegrityReport",
+    "ModelRegistry",
+    "compare_seeded_scores",
+    "rank_models",
+    "summarize_repeated_scores",
+    "validate_dataset",
+]
